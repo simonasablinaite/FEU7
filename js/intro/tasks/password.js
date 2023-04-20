7f9638f9029c6de8d549d51ef6dca08074fb9256
Simona <simonasablinaite@gmail.com>
2023-04-20 21:00:14 +0300

    Task password with trim() and regexp

--- js/intro/tasks/password.js
+++ js/intro/tasks/password.js
@@ -4,7 +4,7 @@
 2.2. Jeigu jis nėra ilgesnis už 20 simbolių, tai parašyti jog: „Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo."
 */
 
-const password = '    5428hyt4i4l2po5572553     ';
+const password = '   542 8hyt4i 4l2po557  2553   ';
 const passwordLength = password.length;
 
 if (passwordLength < 16) {
@@ -17,3 +17,33 @@ if (passwordLength < 16) {
    }
 }
 console.log(passwordLength);
+
+console.log('***antras variantas***');
+const trimmedPassword = password.trim();
+const lengthWithTrim = trimmedPassword.length;
+
+if (lengthWithTrim < 16) {
+   console.log('Slaptažodis yra per trumpas.');
+} else if (lengthWithTrim > 15) {
+   if (lengthWithTrim > 20) {
+      console.log('Slaptažodis tinkamas.');
+   } else {
+      console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.');
+   }
+}
+console.log(lengthWithTrim);
+
+console.log('***trecias variantas***');
+const passwordRegexp = password.replace(/\s/g, '');
+const passwordRegexpLength = passwordRegexp.length;
+
+if (passwordRegexpLength < 16) {
+   console.log('Slaptažodis yra per trumpas.');
+} else if (passwordRegexpLength > 15) {
+   if (passwordRegexpLength > 20) {
+      console.log('Slaptažodis tinkamas.');
+   } else {
+      console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.');
+   }
+}
+console.log(passwordRegexpLength);
