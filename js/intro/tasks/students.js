83f2e390d27e862d645ce382dc7bbf2cfc37da42
Simona <simonasablinaite@gmail.com>
2023-04-21 10:02:52 +0300

    Added min max ages

--- js/intro/tasks/students.js
+++ js/intro/tasks/students.js
@@ -1,18 +1,23 @@
 console.clear();
 
 // Pagal amžių surašyti į kurią klasę eina mokinys:
-let childAge = 6;
+let childAge = 11;
 
-if (childAge <= 6) {
+
+if (childAge < 0) {
+   console.log('įvestas amžius yra per mažas');
+} else if (childAge < 7) {
    console.log('Į mokyklą neina');
-} else if (childAge <= 10) {
+} else if (childAge < 11) {
    console.log('Eina Į pradinę klasę');
-} else if (childAge <= 14) {
+} else if (childAge < 15) {
    console.log('Eina į pagrindinę');
-} else if (childAge <= 18) {
+} else if (childAge < 19) {
    console.log('Eina į gimnaziją');
+} else if (childAge > 120) {
+   console.log('įvestas amžius yra per didelis.');
 } else {
-   console.log('Mokyklą baigė');
+   console.log('Mokyklą baigė.');
 }
 
 
