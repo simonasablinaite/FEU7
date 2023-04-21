a492bae18d754563ec8a8dd82ddd57a17364c4e7
Simona <simonasablinaite@gmail.com>
2023-04-21 09:48:43 +0300

    ChildAge task

---
+++ js/intro/tasks/students.js
@@ -0,0 +1,18 @@
+console.clear();
+
+// Pagal amžių surašyti į kurią klasę eina mokinys:
+let childAge = 6;
+
+if (childAge <= 6) {
+   console.log('Į mokyklą neina');
+} else if (childAge <= 10) {
+   console.log('Eina Į pradinę klasę');
+} else if (childAge <= 14) {
+   console.log('Eina į pagrindinę');
+} else if (childAge <= 18) {
+   console.log('Eina į gimnaziją');
+} else {
+   console.log('Mokyklą baigė');
+}
+
+
