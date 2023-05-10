c45afa8b7531fe1ef896d7d3119daf90c4259410
Simona <simonasablinaite@gmail.com>
2023-05-10 15:02:04 +0300

    Added interests

--- js/intro/tasks/forms.js
+++ js/intro/tasks/forms.js
@@ -1,5 +1,15 @@
 const studentForm = document.querySelector('#student-form');
-
+/*
+TREČIA DALIS:
+1. Vietoje el. pašto rodyti tik žvaigždutes „****".
+2. Kiekviename „student-item" elemente sukurti mygtuką „Rodyti asmens duomenis".
+3. Paspaudus šį mygtuką:
+    3.1. Parodyti to studento el. paštą.
+    3.2. Pakeist mygtuko tekstą į „Slėpti asmens duomenis".
+4. Jeigu asmens duomenys yra rodomi, tai paspaudus mygtuką:
+    4.1. Paslėpti asmens el. paštą.
+    4.2. Mygtuko tekstą pakeisti į „Rodyti asmens duomenis".
+*/
 
 studentForm.addEventListener('submit', (event) => {
    event.preventDefault();
@@ -43,9 +53,33 @@ studentForm.addEventListener('submit', (event) => {
    const groupElement = document.createElement('p');
    groupElement.innerHTML = `<span style='font-weight:900'>Group:</span> ${group}`;
 
-   studentItem.append(nameElement, lastNameElement, ageElement, phoneElement, emailElement, ITknowledgeElement, groupElement) //i studento itema prideda sukurta elementa
 
    studentsList.prepend(studentItem) //i studento lista idedame studento itema
+
+   // checkboxo reiksmiu gavimas:
+   const interests = document.querySelectorAll('[name="interests"]:checked');
+
+   // reiksmes isvedimas i ekrana:
+   const interestsWrapper = document.createElement('div'); //sukuriamas konteineris duomenims talpinti
+   const interestTitle = document.createElement('p'); //sukuriamas duoments kintamasis
+   interestTitle.textContent = 'Strudents interests:'; // suteikiamas tekstas
+
+   const interestList = document.createElement('ul');
+
+   interests.forEach(interest => {
+      const interestElement = document.createElement('li');
+      interestElement.textContent = interest.value;
+      interestList.append(interestElement)
+   })
+
+   interestsWrapper.append(interestTitle, interestList); // kintamasis idedamas i interestu konteineri
+
+
+
+
+
+   studentItem.append(nameElement, lastNameElement, ageElement, phoneElement, emailElement, ITknowledgeElement, groupElement, interestsWrapper) //i studento itema prideda sukurta elementa
+
 })
 
 
