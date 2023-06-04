7cdb6a4ddb9ab5ce96a599e0899191aaf07fddcc
Simona <simonasablinaite@gmail.com>
2023-06-05 00:34:41 +0300

    Registered routes

--- REACT/react-intro/src/App.js
+++ REACT/react-intro/src/App.js
@@ -1,11 +1,24 @@
 import DemoContactsPage from './pages/contacts-us/DemoContactsPage';
-import './pages/news-page/style/style.css'
+import DemoNewsPage from './pages/news-page/DemoNewsPage';
+import './pages/news-page/style/style.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <DemoContactsPage />
+        <Routes>
+          <Route path='/' element={
+            <div>
+              <h1>Cia yra pradinis puslapis.
+                Norint patekti i projektus, ziniatinklio juostoje nurodyk kelius /contacts arba /news</h1>
+            </div>
+          } />
+          <Route path='*' element={<h1>404 Error. Page Not Found</h1>} />
+          <Route path='/contacts' element={<DemoContactsPage />} />
+          <Route path='/news' element={<DemoNewsPage />} />
+        </Routes>
+
       </header>
     </div>
   );
