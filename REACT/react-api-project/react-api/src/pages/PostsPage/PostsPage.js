b84a4a7f4f0b135a57696638bb2550c9d5d75df8
Simona <simonasablinaite@gmail.com>
2023-06-21 09:57:57 +0300

    Posts are returned to the screen

--- REACT/react-api-project/react-api/src/pages/PostsPage/PostsPage.js
+++ REACT/react-api-project/react-api/src/pages/PostsPage/PostsPage.js
@@ -1,40 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const PostsPage = () => {
 
-   const [posts, setPosts] = useState();
+   const [posts, setPosts] = useState([]);
 
    const createPost = () => {
-      fetch('http://localhost:3000/posts', {
-         method: 'POST',
-         body: JSON.stringify({
-            title: 'PAVADINIMAS',
-            body: 'APRASYMAS',
-            userId: 1,
-         }),
-         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-         },
-      })
-         .then((response) => response.json())
-         .then((json) => console.log(json)); // gaunamas atsakymas is backendo
-
-      fetch('https://jsonplaceholder.typicode.com/posts') // paleidziamas dar vienas fetchas gauti visiems post'ams
-         .then(res => res.json())
-         .then(data => console.log(data))
-   }
-   useEffect(() => {
       fetch(`http://localhost:3000/posts`)
          .then(res => res.json())
-         .then(data => {
-            console.log(data);
-         })
+         .then(postsData => {
+            setPosts(postsData);
+         });
+   }
+   useEffect(() => {
+
    }, [])
 
    return (
       <div>
          <h1>Posts:</h1>
          <button onClick={createPost}>Create Post</button>
+
+         <div className='posts-wrapper'>
+            <ul>
+               {posts.map(post => <li key={post.id}><Link to={'/json/posts/' + post.id}>{post.title}</Link></li>)}
+            </ul>
+         </div>
       </div>
    )
 }
