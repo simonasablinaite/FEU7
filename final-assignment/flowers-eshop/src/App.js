435be4592b10ceb82cc2b0da4054ef345fbadeff
Simona <simonasablinaite@gmail.com>
2023-06-23 23:36:15 +0300

    Header

--- final-assignment/flowers-eshop/src/App.js
+++ final-assignment/flowers-eshop/src/App.js
@@ -1,12 +1,49 @@
-import './App.css';
+import { NavLink, Route, Routes, Link } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import './style/header.scss';
+import logo from './img/logo.png';
+import ShopPage from './pages/ShopPage/ShopPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Tegul prasideda svente</h1>
+    <div className="container">
+      <header className="navigation">
+        <nav>
+
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+
+          <div className='photo-wrapper'>
+            <img src={logo} alt="logo" />
+          </div>
+
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+        </nav>
+
+        <select name="languages" id="languages">
+          <option value="lithuanian">lt</option>
+          <option value="lithuanian">en</option>
+        </select>
+
       </header>
-    </div>
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+
+        <Route path='*' element={
+          <div>
+            <h1>404 Error. Page Not Found</h1>
+            <Link to='/'>Go to Home Page</Link>
+          </div>
+        } />
+
+      </Routes>
+    </div >
   );
 }
 
---
+++ final-assignment/flowers-eshop/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+   return (
+      <div className='main-container'>
+         <div className='hero-section'>Hero</div>
+         <div className='shops-list'>Shops list</div>
+         <div className='footer'>Footer</div>
+      </div>
+   )
+}
+
+export default HomePage
