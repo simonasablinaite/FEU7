080dd3acc0f316aa8fe32faa77b3d54444e365c8
Simona <simonasablinaite@gmail.com>
2023-06-23 17:48:33 +0300

    Initialized repo

---
+++ final-assignment/flowers-eshop/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tegul prasideda svente</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
